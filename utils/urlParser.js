26f8c023e100c318591177ce2ee3dc0db5e1d9a5
icaro <icaro@hikaru.2>
2026-04-19 19:12:02 -0300

    fix: url parsing logic

--- utils/urlParser.js
+++ utils/urlParser.js
@@ -11,17 +11,16 @@ function getEmbedVariants(url) {
       url.replace("instagram.com", "ssinstagram.com"),
       url,
     ];
-  }
-
-  if (url.includes("twitter.com") || url.includes("x.com")) {
+  } 
+    if (url.includes("twitter.com") || url.includes("x.com")) {
     return [
       url.replace(/(twitter|x)\.com/, "fxtwitter.com"),
       url.replace(/(twitter|x)\.com/, "vxtwitter.com"),
       url,
     ];
   }
-
-  return [url];
+ 
+    return [url];
 }
 
 export async function urlParser(data, api) {
@@ -31,20 +30,25 @@ export async function urlParser(data, api) {
 
   const urls = extractUrls(data.content);
 
+  console.log("Extracted URLs:", urls);
+
   const supported = urls.filter(
     (url) =>
       url.includes("instagram.com") ||
       url.includes("twitter.com") ||
-      url.includes("x.com")
+      url.includes("//x.com")
   );
+
+    console.log("Supported: " + supported)
 
   if (supported.length === 0) return;
 
   await api.channels.deleteMessage(data.channel_id, data.id);
 
   for (let url of supported) {
+    console.log("Processing URL:", url);
     const variants = getEmbedVariants(url);
-
+    console.log("Embed Variants:", variants);
     await api.channels.createMessage(data.channel_id, {
       content: `${variants[0]}` + "\n" + `By ${data.author.username}`,
     });
