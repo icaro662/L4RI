0c68c640e0762298cac882249eb94d8d21934bde
Ícaro <icarosancho@gmail.com>
2026-09-22 22:31:35 -0300

    refactor: add comments explaining command definition and flow

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,8 +1,19 @@
+/*
+    Command for returning the avatar of a user. If a user is mentioned, it returns their avatar; otherwise, it returns the avatar of the message author.
+    Args:
+    message: The message object from the Fluxer API.
+*/
+
 async function getAvatar(message) {
+
+    // Checks if a user is mentioned in the message. If so, it uses that user's avatar; otherwise, it uses the author's avatar.
     const user = message.mentions?.[0] || message.author;
-    
+
+    // Gets the URL of the user's avatar.
+    // Uses inate Fluxer method that returns the user's avatar URL with a size of 256 pixels.
     const avatarUrl = user.displayAvatarURL({ size: 256 });
     
+    // Sends a reply to the message with an embed containing the user's avatar.
     await message.reply({ 
         ping: false,
         embeds: [
